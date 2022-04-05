cbc543e9f59d5f7a95a838d90ff9287fddfd2ae0
Admin Peter Vantomme <adm677@titeca.be>
2022-04-05 11:36:23 +0200

    updated test file

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
 import base64
 import json
-import zlib
 import requests
 
+login_dict = json.dumps({"username":"dev","password":"secret"})
+body = base64.b64encode(login_dict.encode())
+credentials = requests.post("http://127.0.0.1:1600/token", data=body)
+access_token = str(json.loads(credentials.content))
 with open("HPD201324TC-20220329-101119-002.pdf", "rb") as pdf_file:
     encoded = base64.b64encode(pdf_file.read())
 
-body=bytes(encoded)
-reply=requests.post("http://127.0.0.1:80/data",body)
 
+body=bytes(encoded)
+reply=requests.post(f"http://127.0.0.1:1600/data/{access_token}",data=body)
+
+print(reply.content)
 filename = json.loads(reply.content.decode("utf-8")).get("filename")
 QR_contents = json.loads(reply.content.decode("utf-8")).get("QR_content")
 PDF = json.loads(reply.content.decode("utf-8")).get("Pages")
-file = open(f'datafile.pdf', 'wb')
-file.write(base64.b64decode(PDF))
-print(filename," : ",QR_contents)
-file.close()
+with open(f'datafile.pdf', 'wb') as file:
+    file.write(base64.b64decode(PDF))
+    print(filename," : ",QR_contents)
+
+reply = requests.get("http://127.0.0.1:1600/home", params=str({"token":access_token}))
+print(reply)
